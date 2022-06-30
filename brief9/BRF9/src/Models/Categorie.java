2bffce883e3bd1e7d97ec9dcdd664b72db8c538f
anasmak04 <anasdev8@gmail.com>
2022-06-30 09:26:10 +0200

    not done

---
+++ brief9/BRF9/src/Models/Categorie.java
@@ -0,0 +1,16 @@
+package Models;
+
+public class Categorie {
+	private String nom_categorie;
+
+	public String getNom_categorie() {
+		return nom_categorie;
+	}
+
+	public void setNom_categorie(String nom_categorie) {
+		this.nom_categorie = nom_categorie;
+	}
+	
+	
+	
+}
