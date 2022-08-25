7f3e3f96ba1be60a61db96d9538cffb95b57717e
anasmak04 <anasdev8@gmail.com>
2022-08-25 09:55:43 +0200

    add lombok

--- Brief13-STC-API/Br13/src/main/java/com/project/Br13/entity/Exercice.java
+++ Brief13-STC-API/Br13/src/main/java/com/project/Br13/entity/Exercice.java
@@ -25,6 +25,9 @@ import lombok.ToString;
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 
 public class Exercice {
 	@Id
@@ -33,83 +36,14 @@ public class Exercice {
 	private Long annee;
 	@CreationTimestamp
 	@Column(updatable = true)
-	 Timestamp date_debut;
+	Timestamp date_debut;
 	@CreationTimestamp
 	@Column(updatable = true)
-	 Timestamp date_fin;
+	Timestamp date_fin;
 	private boolean statut;
 
 	@ManyToMany
 	@JoinTable(name = "Exercice_Activite", joinColumns = @JoinColumn(name = "id_exercice "), inverseJoinColumns = @JoinColumn(name = "id_activite"))
 	private List<Activite> activite;
 
-	public Exercice() {
-		super();
-	}
-
-	public Exercice(Long id_exercice, Long annee, Timestamp date_debut, Timestamp date_fin, boolean statut,
-			List<Activite> activite) {
-		super();
-		this.id_exercice = id_exercice;
-		this.annee = annee;
-		this.date_debut = date_debut;
-		this.date_fin = date_fin;
-		this.statut = statut;
-		this.activite = activite;
-	}
-
-	public Long getId_exercice() {
-		return id_exercice;
-	}
-
-	public void setId_exercice(Long id_exercice) {
-		this.id_exercice = id_exercice;
-	}
-
-	public Long getAnnee() {
-		return annee;
-	}
-
-	public void setAnnee(Long annee) {
-		this.annee = annee;
-	}
-
-	public Timestamp getDate_debut() {
-		return date_debut;
-	}
-
-	public void setDate_debut(Timestamp date_debut) {
-		this.date_debut = date_debut;
-	}
-
-	public Timestamp getDate_fin() {
-		return date_fin;
-	}
-
-	public void setDate_fin(Timestamp date_fin) {
-		this.date_fin = date_fin;
-	}
-
-	public boolean isStatut() {
-		return statut;
-	}
-
-	public void setStatut(boolean statut) {
-		this.statut = statut;
-	}
-
-	public List<Activite> getActivite() {
-		return activite;
-	}
-
-	public void setActivite(List<Activite> activite) {
-		this.activite = activite;
-	}
-
-	@Override
-	public String toString() {
-		return "Exercice [id_exercice=" + id_exercice + ", annee=" + annee + ", date_debut=" + date_debut
-				+ ", date_fin=" + date_fin + ", statut=" + statut + ", activite=" + activite + "]";
-	}
-		
 }
