c20c0f666bf8b33372adddcd565a9cb160bd0ff4
anasmak04 <anasdev8@gmail.com>
2022-08-28 15:51:06 +0200

    add csrf

--- Brief13-STC-API/Br13/src/main/java/com/project/Br13/security/AppConfig.java
+++ Brief13-STC-API/Br13/src/main/java/com/project/Br13/security/AppConfig.java
@@ -14,43 +14,30 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class AppConfig extends WebSecurityConfigurerAdapter {
-		
+
 	private final PasswordEncoder passwordencoder;
-	
+
 	public AppConfig(PasswordEncoder passwordencoder) {
 		super();
 		this.passwordencoder = passwordencoder;
 	}
 
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers("/", "index", "/css/*", "/js/*").permitAll()
-		.antMatchers("/admin/**").hasRole(ApplicationUserRole.ADMIN.name())
-				.anyRequest().authenticated().and().httpBasic();
+		http.csrf().disable().authorizeRequests().antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+				.antMatchers("/admin/**").hasRole(ApplicationUserRole.ADMIN.name()).anyRequest().authenticated().and()
+				.httpBasic();
 	}
-	
-	
+
 	@Bean
 	protected UserDetailsService UserDetailsService() {
-			
-			
-			UserDetails Admin = User.builder()
-			.username("admin")
-			.password(passwordencoder.encode("admin123"))
-			.roles(ApplicationUserRole.ADMIN.name())
-			.build();
-			
-			UserDetails JUSTVISITOR = User.builder()
-					.username("visitor")
-					.password(passwordencoder.encode("visitor123"))
-					.roles(ApplicationUserRole.VISITOR.name())
-					.build();
-			
-			return new InMemoryUserDetailsManager(
-					Admin,JUSTVISITOR
-					);
+
+		UserDetails Admin = User.builder().username("admin").password(passwordencoder.encode("admin123"))
+				.roles(ApplicationUserRole.ADMIN.name()).build();
+
+		UserDetails JUSTVISITOR = User.builder().username("visitor").password(passwordencoder.encode("visitor123"))
+				.roles(ApplicationUserRole.VISITOR.name()).build();
+
+		return new InMemoryUserDetailsManager(Admin, JUSTVISITOR);
 	}
-	
-	
-	
+
 }
